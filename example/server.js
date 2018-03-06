8d813c26d8e7a7d371089a30ee3a98f224806312
qm3ster <qm3ster@gmail.com>
2018-03-06 12:14:11 +0200

    Fix typos, consistent style

--- example/server.js
+++ example/server.js
@@ -1,25 +1,25 @@
-var express = require('express')
-  , partialResponse = require('../')
-  , app = express()
+const express = require('express')
+const partialResponse = require('../')
+const app = express()
 
 app.use(partialResponse())
 
-app.get('/', function (res, res, next) {
+app.get('/', function (req, res, next) {
   res.json({
-      firstName: 'Mohandas'
+    firstName: 'Mohandas'
     , lastName: 'Gandhi'
     , aliases: [{
-          firstName: 'Mahatma'
-        , lastName: 'Gandhi'
-      }, {
-          firstName: 'Bapu'
-      }]
+      firstName: 'Mahatma'
+      , lastName: 'Gandhi'
+    }, {
+      firstName: 'Bapu'
+    }]
   })
 })
 
 app.listen(4000, function () {
-  var prefix = 'curl \'http://localhost:4000?fields=%s\''
-  console.log('Server runnong on :4000, try the following:');
+  const prefix = 'curl \'http://localhost:4000?fields=%s\''
+  console.log('Server running on :4000, try the following:')
   console.log(prefix, '*')
   console.log(prefix, 'lastName')
   console.log(prefix, 'firstName,aliases(firstName)')
--- index.js
+++ index.js
@@ -1,36 +1,34 @@
-var jsonMask = require('json-mask'),
-  compile = jsonMask.compile,
-  filter = jsonMask.filter;
+const jsonMask = require('json-mask')
 
 module.exports = function (opt) {
-  opt = opt || {};
+  opt = opt || {}
 
   function partialResponse(obj, fields) {
-    if (!fields) return obj;
-    return filter(obj, compile(fields));
+    if (!fields) return obj
+    return jsonMask(obj, fields)
   }
 
   function wrap(orig) {
     return function (obj) {
-      var param = this.req.query[opt.query || 'fields'];
+      const param = this.req.query[opt.query || 'fields']
       if (1 === arguments.length) {
-        orig(partialResponse(obj, param));
+        orig(partialResponse(obj, param))
       } else if (2 === arguments.length) {
         if ('number' === typeof arguments[1]) {
-          orig(arguments[1], partialResponse(obj, param));
+          orig(arguments[1], partialResponse(obj, param))
         } else {
-          orig(obj, partialResponse(arguments[1], param));
+          orig(obj, partialResponse(arguments[1], param))
         }
       }
-    };
+    }
   }
 
   return function (req, res, next) {
     if (!res.__isJSONMaskWrapped) {
-      res.json = wrap(res.json.bind(res));
-      if (req.jsonp) res.jsonp = wrap(res.jsonp.bind(res));
-      res.__isJSONMaskWrapped = true;
+      res.json = wrap(res.json.bind(res))
+      if (req.jsonp) res.jsonp = wrap(res.jsonp.bind(res))
+      res.__isJSONMaskWrapped = true
     }
-    next();
-  };
-};
+    next()
+  }
+}
